d2e5c89c7c4c751df56e551d23c963326d85787c
jojovibes <86986091+jojovibes@users.noreply.github.com>
2025-04-29 23:48:44 +0200

    implemented palindrome checker

--- palindrome_checker_app/lib/main.dart
+++ palindrome_checker_app/lib/main.dart
@@ -30,10 +30,22 @@ class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _controller = TextEditingController();
   String _result = '';
 
-   void _checkPalindrome() {
-    //function to check if the input is a palindrome
-    String input = _controller.text;
-   }
+  bool _isPalindrome(String input) {
+    final cleaned = input.toLowerCase().replaceAll(RegExp(r'[^a-z0-9]'), ''); // preprocessing the input (removes all non-alphanumeric characters and converts to lowercase)
+    return cleaned == cleaned.split('').reversed.join(); //checks if the cleaned string is equal to its reverse
+  }
+
+  void _checkPalindrome() {
+    final input = _controller.text;
+    final isPalindrome = _isPalindrome(input); //actual check
+    final result = isPalindrome ? '✅ "$input" is a palindrome.' : '❌ "$input" is not a palindrome.';
+
+    setState(() {
+      _result = result;
+    }); // updates state
+
+    _controller.clear(); //clears the text field
+  }
 
   @override
   Widget build(BuildContext context) {
